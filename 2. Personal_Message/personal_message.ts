3d2be33f0d5a3355f4f96daea8e572385ed311eb
Fatima-229 <159787658+Fatima-229@users.noreply.github.com>
2024-04-29 21:32:30 +0500

    second commit

---
+++ 2. Personal_Message/personal_message.ts
@@ -0,0 +1,7 @@
+let PersonName: string = "";
+PersonName = prompt("What is your name?") || "";
+if (PersonName !== null && PersonName !== "") {
+  alert(`Hello ${PersonName}, would you like to learn some Python today?`);
+} else {
+  alert("You have to fill your name!");
+}
